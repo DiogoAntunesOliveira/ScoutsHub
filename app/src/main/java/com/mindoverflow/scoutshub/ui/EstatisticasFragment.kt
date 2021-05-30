ac498bada6f58c10986497eca3c13562b5e1df49
Diogo Antunes Oliveira <diogojose11@live.com>
2021-05-30 01:03:35 +0100

    Bottom navigation Bar update

--- app/src/main/java/com/mindoverflow/scoutshub/ui/EstatisticasFragment.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/EstatisticasFragment.kt
@@ -22,29 +22,6 @@ class EstatisticasFragment : Fragment() {
 
         val rootView = inflater.inflate(R.layout.fragment_estatisticas, container, false)
 
-        val graph = rootView.findViewById<GraphView>(R.id.graph)
-
-        var x = -5.0
-        var y = 0.0
-
-        val series = LineGraphSeries<DataPoint>()
-        val gridLabel: GridLabelRenderer = graph.gridLabelRenderer
-
-        graph.title = "Percentagem de atividades participadas desde o inicio"
-        graph.titleTextSize = 35F
-
-        gridLabel.horizontalAxisTitle = "X Axis Title"
-        gridLabel.verticalAxisTitle = "Y Axis Title"
-
-
-        for(i in 0..200){
-            x += 0.1
-            y = sin(x)
-            series.appendData(DataPoint(x, y), true, 500)
-        }
-
-        graph.addSeries(series)
-
 
         return rootView
     }
