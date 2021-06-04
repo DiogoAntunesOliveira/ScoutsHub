136c108f387533c3a938dec99e29ed01502a7c11
a18094ipca <a18094@alunos.ipca.pt>
2021-06-04 21:49:51 +0100

    Substituição do CalendarView para um MaterialCalendarView em activity_adicionar_atividade e na classe NovaAtividadeActivity.kt

--- app/src/main/java/com/mindoverflow/scoutshub/ui/NovaAtividadeActivity.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/NovaAtividadeActivity.kt
@@ -1,39 +1,53 @@
 package com.mindoverflow.scoutshub.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.mindoverflow.scoutshub.R
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class NovaAtividadeActivity : AppCompatActivity() {
 
     var dataAtividade : String = ""
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicionar_atividade)
 
-        val texto = findViewById<TextView>(R.id.textView2)
+        supportActionBar!!.hide()
 
+        val textocomdate = findViewById<TextView>(R.id.diaEscolhido)
 
-
-
-        val calendarView = findViewById<CalendarView>(R.id.calendarView)
-
-        calendarView.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
-            val realmonth = month +1
-            dataAtividade = "$dayOfMonth/$realmonth/$year"
-        })
-
-
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val date = formatter.parse(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+        val dataatual = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(date)
+        textocomdate.text = dataatual
         val editTextNomeCompleto = findViewById<EditText>(R.id.editTextNomeCompleto)
         val editTextDescricao = findViewById<EditText>(R.id.editTextDescricao)
+
+        val calendarView = findViewById<com.applandeo.materialcalendarview.CalendarView>(R.id.calendarView)
+
+
+        calendarView.setOnDayClickListener { eventDay ->
+            val clickedDateMilis: Long = eventDay.calendar.timeInMillis
+
+            val datemilitodate = Date(clickedDateMilis)
+            val dateformatter = SimpleDateFormat("dd/MM/yyyy")
+            textocomdate.text = dateformatter.format(datemilitodate)
+
+            dataAtividade = dateformatter.format(datemilitodate)
+            println(dataAtividade)
+        }
 
 
         findViewById<Button>(R.id.buttonNextNewActivity).setOnClickListener{
