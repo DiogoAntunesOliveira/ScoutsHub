cf9034e08ee449efd52f0da16a6eefa5558ea3a9
JMFS4 <jorgemiguelsa@zonmail.pt>
2021-06-14 00:55:12 +0100

    Emplementacao do tipo de pedido GET

--- app/src/main/java/com/mindoverflow/scoutshub/models/Perfil.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/models/Perfil.kt
@@ -6,7 +6,7 @@ class Perfil {
 
     var idPerfil                : Int?                 = null
     var nome                    : String?              = null
-    var imagem                  : String?              = null
+    //var imagem                  : String?              = null
     var dtNasc                  : String?              = null
     var genero                  : String?              = null
     var contacto                : Int?                 = null
@@ -25,7 +25,7 @@ class Perfil {
     constructor(
         idPerfil                        : Int?,
         nome                            : String?,
-        imagem                          : String?,
+        //imagem                          : String?,
         dtNasc                          : String?,
         genero                          : String?,
         contacto                        : Int?,
@@ -37,7 +37,7 @@ class Perfil {
     ) {
         this.idPerfil                   = idPerfil
         this.nome                       = nome
-        this.imagem                     = imagem
+        //this.imagem                     = imagem
         this.dtNasc                    = dtNasc
         this.genero                     = genero
         this.contacto                   = contacto
@@ -53,7 +53,7 @@ class Perfil {
 
         jsonObject.put("id_perfil"                          , idPerfil          )
         jsonObject.put("nome"                               , nome              )
-        jsonObject.put("imagem"                             , imagem              )
+        //jsonObject.put("imagem"                             , imagem              )
         jsonObject.put("data_nascimento"                    , dtNasc           )
         jsonObject.put("genero"                             , genero            )
         jsonObject.put("contacto"                           , contacto          )
@@ -71,14 +71,14 @@ class Perfil {
             val perfil = Perfil()
             perfil.idPerfil             = if (!jsonObject.isNull("id_perfil"                        )) jsonObject.getInt        ("id_perfil"                        )   else null
             perfil.nome                 = if (!jsonObject.isNull("nome"                             )) jsonObject.getString     ("nome"                             )   else null
-            perfil.imagem               = if (!jsonObject.isNull("imagem"                           )) jsonObject.getString     ("imagem"                           )   else null
-            perfil.dtNasc              = if (!jsonObject.isNull("data_nascimento"                  )) jsonObject.getString     ("data_nascimento"                  )   else null
+            //perfil.imagem               = if (!jsonObject.isNull("imagem"                           )) jsonObject.getString     ("imagem"                           )   else null
+            perfil.dtNasc              = if (!jsonObject.isNull("dt_nasc"                           )) jsonObject.getString     ("dt_nasc"                  )           else null
             perfil.genero               = if (!jsonObject.isNull("genero"                           )) jsonObject.getString     ("genero"                           )   else null
             perfil.contacto             = if (!jsonObject.isNull("contacto"                         )) jsonObject.getInt        ("contacto"                         )   else null
             perfil.morada               = if (!jsonObject.isNull("morada"                           )) jsonObject.getString     ("morada"                           )   else null
             perfil.codigoPostal         = if (!jsonObject.isNull("codigo_postal"                    )) jsonObject.getString     ("codigo_postal"                    )   else null
-            perfil.nin                  = if (!jsonObject.isNull("numero_identificacao_fiscal"      )) jsonObject.getInt        ("numero_identificacao_fiscal"      )   else null
-            perfil.totalAtivParticip    = if (!jsonObject.isNull("total_atividades_participadas"    )) jsonObject.getInt        ("total_atividades_participadas"    )   else null
+            perfil.nin                  = if (!jsonObject.isNull("nin"                              )) jsonObject.getInt        ("nin"      )                           else null
+            perfil.totalAtivParticip    = if (!jsonObject.isNull("total_atividades_part"            )) jsonObject.getInt        ("total_atividades_part"    )            else null
             perfil.idEquipa             = if (!jsonObject.isNull("id_equipa"                        )) jsonObject.getInt        ("id_equipa"                        )   else null
 
             return perfil
--- app/src/main/java/com/mindoverflow/scoutshub/ui/PerfisFragmentUtilizador.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/PerfisFragmentUtilizador.kt
@@ -12,6 +12,14 @@ import com.mindoverflow.scoutshub.R
 import com.mindoverflow.scoutshub.adapter.CustomAdapter
 import com.mindoverflow.scoutshub.models.Atividade
 import com.mindoverflow.scoutshub.models.Perfil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 
 class PerfisFragmentUtilizador : Fragment() {
@@ -20,11 +28,12 @@ class PerfisFragmentUtilizador : Fragment() {
     //lateinit var adapter : PerfilAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
         val rootView = inflater.inflate(R.layout.fragment_perfil_utilizador, container, false)
+
 
         val image0 = R.drawable.bryce_canyon
         val image1 = R.drawable.cathedral_rock
@@ -34,28 +43,12 @@ class PerfisFragmentUtilizador : Fragment() {
 
         val images = arrayListOf(image0, image1, image2, image3, image4)
 
-        val nomeUtilizador = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorNome)
-        val dtNasc = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorDataNasc)
-        val genero = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorGenero)
-        val contacto = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorContacto)
-        val morada = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorMorada)
-        val codigoPostal = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorCodigoPostal)
-        val nin = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorNin)
-        val totalAtivParticip = rootView.findViewById<TextView>(R.id.textViewPerfilUtilizadorTotalAtivParticip)
 
-        val perfil = Perfil(null, "Jorge", "dfgdfgrwe", "30/5/2000", "M", 919923205, "Praceta Madalena Fonseca 120 Rés do chão", "9560-010", 123456789, 6, 5)
+        //val perfil = Perfil(null, "Jorge", "30/5/2000", "M", 919923205, "Praceta Madalena Fonseca 120 Rés do chão", "9560-010", 123456789, 6, 5)
 
-        nomeUtilizador.text = perfil.nome
-        dtNasc.text = perfil.dtNasc
-        genero.text = perfil.genero
-        contacto.text = perfil.contacto.toString()
-        morada.text = perfil.morada
-        codigoPostal.text = perfil.codigoPostal
-        nin.text = perfil.nin.toString()
-        totalAtivParticip.text = perfil.totalAtivParticip.toString()
 
         //getting recyclerview from xml
-        val recyclerView = rootView.findViewById(R.id.recyclerViewProfile) as RecyclerView
+        val recyclerView = rootView!!.findViewById(R.id.recyclerViewProfile) as RecyclerView
 
         //adding a layoutmanager
         //val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
@@ -89,6 +82,51 @@ class PerfisFragmentUtilizador : Fragment() {
         recyclerView.adapter = adapter
 
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nomeUtilizador = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorNome)
+        val dtNasc = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorDataNasc)
+        val genero = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorGenero)
+        val contacto = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorContacto)
+        val morada = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorMorada)
+        val codigoPostal = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorCodigoPostal)
+        val nin = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorNin)
+        val totalAtivParticip = view.findViewById<TextView>(R.id.textViewPerfilUtilizadorTotalAtivParticip)
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+
+            val id = 1
+
+            val client = OkHttpClient()
+
+            val request = Request.Builder().url("http://mindoverflow.amipca.xyz:60000/perfil/$id").get().build()
+
+            client.newCall(request).execute().use { response ->
+                val jsStr = (response.body!!.string())
+
+                val jsonArray = JSONObject(jsStr).getJSONArray("perfis")
+
+                val jsonObject = JSONObject(jsonArray[0].toString())
+
+                val perfilFromJson = Perfil.fromJson(jsonObject)
+
+
+                GlobalScope.launch(Dispatchers.Main) {
+                    nomeUtilizador.text = perfilFromJson.nome
+                    dtNasc.text = perfilFromJson.dtNasc
+                    genero.text = perfilFromJson.genero
+                    contacto.text = perfilFromJson.contacto.toString()
+                    morada.text = perfilFromJson.morada
+                    codigoPostal.text = perfilFromJson.codigoPostal
+                    nin.text = perfilFromJson.nin.toString()
+                    totalAtivParticip.text = perfilFromJson.totalAtivParticip.toString()
+                }
+            }
+        }
     }
 
     /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
