a0ee087119a983295d38d640b902db0bbf2fdc23
DiogoAntunesOliveira <diogojose11@live.com>
2021-06-14 12:48:22 +0100

    MapsActivity

---
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/Atividades/SampleMapsActivity.kt
@@ -0,0 +1,15 @@
+package com.mindoverflow.scoutshub.ui.Atividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mindoverflow.scoutshub.R
+
+class SampleMapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_maps)
+
+
+
+    }
+}
