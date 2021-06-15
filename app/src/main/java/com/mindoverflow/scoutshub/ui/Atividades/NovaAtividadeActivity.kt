2116f5466e168827f010ee8abf6211e150c4d2e3
a18094ipca <a18094@alunos.ipca.pt>
2021-06-15 11:53:34 +0100

    Estatisticas conecta-se colecta coordenadas GPS e Localização e conecta-se com  uma Weather API Externa mostrando assim a temperatura Calendario obtem Atividades e adiciona-os no calendario como eventos Nova Atividade tem agora um botão para selecionar hora

--- app/src/main/java/com/mindoverflow/scoutshub/ui/Atividades/NovaAtividadeActivity.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/Atividades/NovaAtividadeActivity.kt
@@ -1,18 +1,23 @@
-package com.mindoverflow.scoutshub.ui.Atividades
+package com.mindoverflow.scoutshub.ui
 
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.TimePicker
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.mindoverflow.scoutshub.R
+import com.mindoverflow.scoutshub.ui.Atividades.ConfirmNewActivity
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.time.hours
 
 
 class NovaAtividadeActivity : AppCompatActivity() {
@@ -46,9 +51,35 @@ class NovaAtividadeActivity : AppCompatActivity() {
             val dateformatter = SimpleDateFormat("dd/MM/yyyy")
 
             dataAtividade = dateformatter.format(datemilitodate)
+            findViewById<TextView>(R.id.textViewData).setText(dataAtividade)
         }
 
+        findViewById<Button>(R.id.buttonHora).setOnClickListener{
 
+            // Cria uma instancia OnTimeSetListener , a instancia ira desaparecer apos clicar-se no okay ao selecionar a hora
+            val onTimeSetListener = TimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hour: Int, minute: Int ->
+
+                var hourfinal = "$hour:$minute"
+                val timePickerValueTextView = findViewById<TextView>(R.id.textViewHora);
+                timePickerValueTextView.setText(hourfinal);
+            }
+
+            val now = Calendar.getInstance();
+            var hour = now.get(Calendar.HOUR)
+            if (hour <10){hour = Integer.parseInt("0$hour")}
+            var minute = now.get(Calendar.MINUTE)
+
+            // Whether show time in 24 hour format or not.
+            val is24Hour = true;
+
+            val timePickerDialog = TimePickerDialog(this,
+                onTimeSetListener,
+                hour,
+                minute,
+                is24Hour);
+
+            timePickerDialog.show();
+        }
 
         //Ao clicar-se no botão Next é verificado se os dois editTexts são diferentes de null
         findViewById<Button>(R.id.buttonNextNewActivity).setOnClickListener{
@@ -58,15 +89,15 @@ class NovaAtividadeActivity : AppCompatActivity() {
                 //Se não forem null é criado um Intent com o contexto como esta Activity e a proxima activity como classe
                 val intent = Intent(this, ConfirmNewActivity::class.java)
                 //De seguida é colocado as strings de informação (data , nome e descricao) dentro da string com os seus devidos ids/nomes
-                            intent.putExtra("dataAtividade", dataAtividade)
-                            intent.putExtra("nomeCompleto", editTextNomeCompleto.text.toString())
-                            intent.putExtra("descricao", editTextDescricao.text.toString())
-                            //Inicia a proxima actividade
-                            this.startActivity(intent)
+                intent.putExtra("dataAtividade", dataAtividade)
+                intent.putExtra("nomeCompleto", editTextNomeCompleto.text.toString())
+                intent.putExtra("descricao", editTextDescricao.text.toString())
+                //Inicia a proxima actividade
+                this.startActivity(intent)
 
             }
 
-       }
+        }
     }
 
 }
