2ed4ed0c2168151d31918acb666e2b9157b9728a
Diogo Antunes Oliveira <diogojose11@live.com>
2021-06-24 12:36:19 +0100

    button accepeted request

--- app/src/main/java/com/mindoverflow/scoutshub/ui/Atividades/AvailableActivitiesActivity.kt
+++ app/src/main/java/com/mindoverflow/scoutshub/ui/Atividades/AvailableActivitiesActivity.kt
@@ -7,23 +7,25 @@ package com.mindoverflow.scoutshub.ui.Atividades
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import bit.linux.tinyspacex.Helpers.getURL
 import com.mindoverflow.scoutshub.MapsActivity
 import com.mindoverflow.scoutshub.R
+import com.mindoverflow.scoutshub.SavedUserData
 import com.mindoverflow.scoutshub.adapter.CustomAdapter
 import com.mindoverflow.scoutshub.models.Atividade
+import com.mindoverflow.scoutshub.models.Participante
 import com.mindoverflow.scoutshub.ui.Login.FrontPage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
@@ -99,6 +101,7 @@ class AvailableActivitiesActivity : AppCompatActivity() {
             var cardBeginData = rowView.findViewById<TextView>(R.id.BeginDataTextView)
             var cardOverData = rowView.findViewById<TextView>(R.id.OverDataTextView)
             val cardImageView = rowView.findViewById<ImageView>(R.id.imageViewCard)
+            val buttonAcepetRequest = rowView.findViewById<ImageView>(R.id.buttonCardAvailableActivityAccept)
 
             cardTitle.text = cardAvalableActivities[position].nome.toString()
             cardType.text = cardAvalableActivities[position].tipo.toString()
@@ -110,6 +113,56 @@ class AvailableActivitiesActivity : AppCompatActivity() {
                 intent.putExtra("LOCATION_LATITUDE", cardAvalableActivities[position].latitude)
                 intent.putExtra("LOCATION_LONGITUDE", cardAvalableActivities[position].longitude)
                 startActivity(intent)
+            }
+
+            buttonAcepetRequest.setOnClickListener{
+
+                GlobalScope.launch(Dispatchers.IO) {
+                    val client = OkHttpClient()
+                    print("teste1")
+
+                    // ATENTION: here i create a new constructor in Atividade class for only send
+                    // post with confirmation
+                    val participant = Participante(
+                        confirmacao = 1
+                    )
+                    print("teste2")
+
+                    // Create body for post in participant
+                    val requestBody = RequestBody.create(
+                        "application/json".toMediaTypeOrNull(),
+                        participant.toJson().toString()
+                    )
+
+                    println(getURL() + "/participant/atividade/" +
+                            cardAvalableActivities[position].idAtividade
+                            + "/utilizador/" + SavedUserData.id_utilizador)
+
+                    // Send and post previous body
+                    val request = Request.Builder()
+                        .url(
+                            getURL() + "/participant/atividade/" +
+                                    cardAvalableActivities[position].idAtividade
+                                    + "/utilizador/" + SavedUserData.id_utilizador
+                        )
+                        .post(requestBody)
+                        .build()
+
+                    // Verification if succesfull
+                    client.newCall(request).execute().use { response ->
+                        Log.d("participante", response.message)
+
+                        GlobalScope.launch(Dispatchers.Main) {
+                            if (response.message == "OK"){
+                                println("Successfully posted")
+                            }else{
+                                Toast.makeText(this@AvailableActivitiesActivity,
+                                    "Ja esta inscrito nesta atividade", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                }
             }
 
             return  rowView
